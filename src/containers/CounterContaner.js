84efc543cbb1d622c8638adabc570d1ee27c29a5
ddayooung <dimmaculatey@gmail.com>
2020-09-10 08:16:26 +0900

    mapStateToProps, mapDispatchToProps

--- src/containers/CounterContaner.js
+++ src/containers/CounterContaner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
